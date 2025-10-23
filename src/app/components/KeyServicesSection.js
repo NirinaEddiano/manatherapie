bb825915c8288074ebf8b1c23d4ca18cb3ecf8cb
NirinaEddiano <anoeddi84@gmail.com>
2025-10-23 10:22:13 +0000

    Update KeyServicesSection.js

--- src/app/components/KeyServicesSection.js
+++ src/app/components/KeyServicesSection.js
@@ -64,7 +64,7 @@ const AccordionItem = ({ service, isOpen, onClick }) => {
           >
             <div className="p-8 pt-0 grid grid-cols-1 md:grid-cols-3 gap-8"> 
               <div className="relative h-48 md:h-full rounded-xl overflow-hidden md:col-span-1"> 
-                <Image src={service.image} alt={service.title} layout="fill" objectFit="cover" />
+                <Image src={service.image} alt={service.title} fill className="object-cover"/>
               </div>
               <div className="flex flex-col justify-center md:col-span-2">
                 <p className="text-xl text-gray-700 mb-6">{service.description}</p> 
